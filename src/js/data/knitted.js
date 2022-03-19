f362488e8ef655b1bf710d5126bb8035d8b7b972
Vladimir Tonkov <vovatonkov@yandex.ru>
2022-03-19 18:47:35 +0400

    added new properties (sizes and pictures) to each product

--- src/js/data/knitted.js
+++ src/js/data/knitted.js
@@ -1,19 +1,43 @@
 export default [
     {
-        img: '../img/knits/knit-1.webp',
-        title: 'Turtleneck Knit Jumper',
+        id: '1',
+        img: '../img/knits/knit-1.jpg',
+        title: 'Knit Sweater',
         price: '5200 руб.',
-        color: 'gray',
-        size: 's'
+        color: 'green',
+        size: 's',
+        moreImagesProduct: [
+            '../img/productsImg/knits/1/knit-1.jpg',
+            '../img/productsImg/knits/1/knit-2.jpg',
+            '../img/productsImg/knits/1/knit-3.jpg',
+            '../img/productsImg/knits/1/knit-4.jpg',
+            '../img/productsImg/knits/1/knit-5.jpg',
+            '../img/productsImg/knits/1/knit-6.jpg',
+            // '../img/productsImg/knits/1/knit-7.jpg'
+        ],
+        availableSizes: [
+            'xs', 's', 'm', 'l'
+        ]
     },
     {
-        img: '../img/knits/knit-2.webp',
+        id: '2',
+        img: '../img/knits/knit-2.jpg',
         title: 'Voluminous Turtleneck Knit Jumper',
         price: '500 руб.',
         color: 'gray',
-        size: "m"
+        size: "m",
+        moreImagesProduct: [
+            '../img/productsImg/knits/2/knit-1.jpg',
+            '../img/productsImg/knits/2/knit-2.jpg',
+            '../img/productsImg/knits/2/knit-3.jpg',
+            '../img/productsImg/knits/2/knit-4.jpg',
+            '../img/productsImg/knits/2/knit-5.jpg',
+            '../img/productsImg/knits/2/knit-6.jpg',
+            '../img/productsImg/knits/2/knit-7.jpg'
+        ]
     },
     {
+        id: '3',
         img: '../img/knits/knit-3.webp',
         title: 'Striped Knit Sweater',
         price: '3700 руб.',
@@ -21,6 +45,7 @@ export default [
         size: 's'
     },
     {
+        id: '4',
         img: '../img/knits/knit-4.webp',
         title: 'Cable Knit Sweater',
         price: '1200 руб.',
@@ -28,6 +53,7 @@ export default [
         size: 'l'
     },
     {
+        id: '5',
         img: '../img/knits/knit-5.webp',
         title: 'Fitted Rib Knit Top',
         price: '1660руб.',
@@ -35,6 +61,7 @@ export default [
         size: 's'
     },
     {
+        id: '6',
         img: '../img/knits/knit-6.webp',
         title: 'Fitted Rib Knit Top',
         price: '1660руб.',
@@ -42,6 +69,7 @@ export default [
         size: 'xxl'
     },
     {
+        id: '7',
         img: '../img/knits/knit-7.webp',
         title: 'Mock Neck Sweater',
         price: '360руб.',
