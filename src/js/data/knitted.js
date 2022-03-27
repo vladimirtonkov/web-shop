be7b263add7f381049fb63b684c64aa878d4b257
Vladimir Tonkov <vovatonkov@yandex.ru>
2022-03-27 21:39:12 +0400

    fix - added new propertiesа

--- src/js/data/knitted.js
+++ src/js/data/knitted.js
@@ -17,7 +17,15 @@ export default [
         ],
         availableSizes: [
             'xs', 's', 'm', 'l'
-        ]
+        ],
+        colorList: {
+            "Taupe": "../img/productsImg/knits/1/colors/taupe.jpg",
+            "Light-belge": "../img/productsImg/knits/1/colors/light-belge.jpg",
+            "Black": "../img/productsImg/knits/1/colors/black.jpg",
+            "Green": "../img/productsImg/knits/1/colors/green.jpg",
+            "Dark-taupe": "../img/productsImg/knits/1/colors/dark-taupe.jpg",
+            "Cream": "../img/productsImg/knits/1/colors/cream.jpg"
+        }
     },
     {
         id: '2',
@@ -53,38 +61,174 @@ export default [
         title: 'Striped Knit Sweater',
         price: '3700 руб.',
         color: 'gray',
-        size: 's'
+        size: 's',
+        moreImagesProduct: [
+            '../img/productsImg/knits/3/knit-1.jpg',
+            '../img/productsImg/knits/3/knit-2.jpg',
+            '../img/productsImg/knits/3/knit-3.jpg',
+            '../img/productsImg/knits/3/knit-4.jpg',
+            '../img/productsImg/knits/3/knit-5.jpg',
+            '../img/productsImg/knits/3/knit-6.jpg',
+            // '../img/productsImg/knits/1/knit-7.jpg'
+        ],
+        availableSizes: [
+            's', 'm', 'l'
+        ],
+        colorList: {
+            "Taupe": "../img/productsImg/knits/3/colors/taupe.jpg",
+            "Gray": "../img/productsImg/knits/3/colors/light-belge.jpg",
+            "Black": "../img/productsImg/knits/3/colors/black.jpg",
+            "Green": "../img/productsImg/knits/3/colors/green.jpg",
+            "Dark-taupe": "../img/productsImg/knits/3/colors/dark-taupe.jpg",
+            "Cream": "../img/productsImg/knits/3/colors/cream.jpg"
+        }
     },
     {
         id: '4',
         img: '../img/knits/knit-4.webp',
         title: 'Cable Knit Sweater',
         price: '1200 руб.',
-        color: 'gray',
-        size: 'l'
+        color: 'light-blue',
+        size: 'l',
+        moreImagesProduct: [
+            '../img/productsImg/knits/4/knit-1.jpg',
+            '../img/productsImg/knits/4/knit-2.jpg',
+            '../img/productsImg/knits/4/knit-3.jpg',
+            '../img/productsImg/knits/4/knit-4.jpg',
+            '../img/productsImg/knits/4/knit-5.jpg',
+            '../img/productsImg/knits/4/knit-6.jpg',
+        ],
+        availableSizes: [
+            'xs', 's', 'm', 'l'
+        ],
+        colorList: {
+            "Light-blue": "../img/productsImg/knits/4/colors/light-blue.jpg",
+            "Dark-turquolse": "../img/productsImg/knits/4/colors/dark-turquolse.jpg",
+            "Bright-blue": "../img/productsImg/knits/4/colors/bright-blue.jpg",
+            "Light-green": "../img/productsImg/knits/4/colors/light-green.jpg",
+        }
     },
     {
         id: '5',
         img: '../img/knits/knit-5.webp',
         title: 'Fitted Rib Knit Top',
         price: '1660руб.',
-        color: 'green',
-        size: 's'
+        color: 'navy-black',
+        size: 's',
+        moreImagesProduct: [
+            '../img/productsImg/knits/5/knit-1.jpg',
+            '../img/productsImg/knits/5/knit-2.jpg',
+            '../img/productsImg/knits/5/knit-3.jpg',
+            '../img/productsImg/knits/5/knit-4.jpg',
+            '../img/productsImg/knits/5/knit-5.jpg',
+            '../img/productsImg/knits/5/knit-6.jpg',
+        ],
+        availableSizes: [
+            'xs', 's', 'm', 'l'
+        ],
+        colorList: {
+            "Light-belge-melange": "../img/productsImg/knits/5/colors/light-belge-melange.jpg",
+            "Light-gray-melange": "../img/productsImg/knits/5/colors/light-gray-melange.jpg",
+            "Navy-black": "../img/productsImg/knits/5/colors/navy-black.jpg",
+
+        }
     },
     {
         id: '6',
         img: '../img/knits/knit-6.webp',
         title: 'Fitted Rib Knit Top',
         price: '1660руб.',
-        color: 'green',
-        size: 'xxl'
+        color: 'belge',
+        size: 'xxl',
+        moreImagesProduct: [
+            '../img/productsImg/knits/6/knit-1.jpg',
+            '../img/productsImg/knits/6/knit-2.jpg',
+            '../img/productsImg/knits/6/knit-3.jpg',
+            '../img/productsImg/knits/6/knit-4.jpg',
+            '../img/productsImg/knits/6/knit-5.jpg',
+            '../img/productsImg/knits/6/knit-6.jpg',
+        ],
+        availableSizes: [
+            'm', 'l', 'xxl'
+        ],
+        colorList: {
+            "belge": "../img/productsImg/knits/6/colors/belge.jpg",
+            "light-gray": "../img/productsImg/knits/6/colors/light-gray.jpg",
+        }
     },
     {
         id: '7',
         img: '../img/knits/knit-7.webp',
         title: 'Mock Neck Sweater',
         price: '360руб.',
-        color: 'yellow',
-        size: 'xxl'
+        color: 'light-gray',
+        size: 'xxl',
+        moreImagesProduct: [
+            '../img/productsImg/knits/7/knit-1.jpg',
+            '../img/productsImg/knits/7/knit-2.jpg',
+            '../img/productsImg/knits/7/knit-3.jpg',
+            '../img/productsImg/knits/7/knit-4.jpg',
+            '../img/productsImg/knits/7/knit-5.jpg',
+            '../img/productsImg/knits/7/knit-6.jpg',
+        ],
+        availableSizes: [
+            'l', 'xxl'
+        ],
+        colorList: {
+            "black": "../img/productsImg/knits/7/colors/black.jpg",
+            "black-white": "../img/productsImg/knits/7/colors/black-white.jpg",
+            "light-belge": "../img/productsImg/knits/7/colors/light-belge.jpg",
+            "light-gray": "../img/productsImg/knits/7/colors/light-gray.jpg",
+            "yellow": "../img/productsImg/knits/7/colors/yellow.jpg",
+        }
     },
 ]
+
+
+// export default [
+//     {
+//         id: '1',
+//         img: '../img/shoes/knit-1.jpg',
+//         imgHover: '../img/shoes/knit-1-2.jpg',
+//         title: 'Braided Slides',
+//         price: '1250 руб.',
+//         color: 'black',
+//         size: '36',
+//     },
+//     {
+//         id: '2',
+//         img: '../img/shoes/knit-2.jpg',
+//         imgHover: '../img/shoes/knit-2.jpg',
+//         title: 'Sandals',
+//         price: '2000 руб.',
+//         color: 'brown',
+//         size: "38",
+//     },
+//     {
+//         id: '3',
+//         img: '../img/knits/knit-3.jpg',
+//         imgHover: '../img/knits/knit-3-2.jpg',
+//         title: 'Espadrille Sneakers',
+//         price: '3700 руб.',
+//         color: 'white',
+//         size: '36',
+//     },
+//     {
+//         id: '4',
+//         img: '../img/knits/knit-4.jpg',
+//         imgHover: '../img/knits/knit-4-2.jpg',
+//         title: 'Chunky Loafers',
+//         price: '1200 руб.',
+//         color: 'Black',
+//         size: '39',
+//     },
+//     {
+//         id: '5',
+//         img: '../img/knits/knit-5.jpg',
+//         imgHover: '../img/knits/knit-5-2.jpg',
+//         title: 'Chunky Sneakers',
+//         price: '4160руб.',
+//         color: 'gray',
+//         size: 's',
+//     },
+// ]
